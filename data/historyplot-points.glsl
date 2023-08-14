21c65a5987dd4407fd5103b40aef8b06941a5529
julesb <julesb@users.noreply.github.com>
2023-08-14 21:01:30 +0930

    antialiased plot lines

---
+++ data/historyplot-points.glsl
@@ -0,0 +1,38 @@
+uniform vec2 resolution;
+uniform float values[512];
+
+float exp_glow(vec2 uv, float e) {
+    return e / length(uv);
+}
+
+
+void main(void) {
+    vec2 ar = vec2(resolution.x/resolution.y, 1.0);
+    vec2 uv = (gl_FragCoord.xy / resolution) * ar;
+
+    vec3 col = vec3(0.0, 0.0, 0.0);
+    
+    int idx = int(uv.x/ar.x*512.0) % int(512);
+    float value = values[idx];
+    
+    float currentValue = values[511];
+
+    // fill area under curve
+    // if (uv.y < value) {
+    //   col.g = 0.5;
+    // }
+    
+    vec2 vpos = vec2(uv.x,  value);
+    
+    // the point
+    //col = vec3(exp_glow(uv - vpos, 0.001));
+    col = vec3(smoothstep(1.0/resolution.y, 0., length(uv - vpos))) * vec3(0.2, 0.96, 0.8);
+
+    // red dot at center - placeholder out-of-range indicator
+    if (currentValue > 1.0 - (2.0 / resolution.y)) {
+      col.r += exp_glow(uv - vec2(0.5, 0.5)*ar, 0.02);
+    }
+
+    gl_FragColor = vec4(col, 0.5);
+}
+
--- data/historyplot.glsl
+++ data/historyplot.glsl
@@ -1,41 +1,41 @@
 uniform vec2 resolution;
-uniform float time;
-uniform float zoom;
 uniform float values[512];
-uniform float valuesindex; // start index
-
-float exp_glow(vec2 uv, float e) {
-    return e / length(uv);
-}
-
 
 void main(void) {
-    vec2 ar = vec2(resolution.x/resolution.y, 1.0);
-    vec2 uv = (gl_FragCoord.xy / resolution) * ar;
+  vec2 ar = vec2(resolution.x / resolution.y, 1.0);
+  vec2 uv = (gl_FragCoord.xy / resolution) * ar;
+  vec4 col = vec4(0.0, 0.0, 0.0, 1.0);
+  
+  //float lineThickness = 1.25 / 300.0;
+  float lineThickness = 1.5 / resolution.y;
 
-    vec3 col = vec3(0.0, 0.0, 0.0);
-    
-    int idx = int(uv.x/ar.x*512.0) % int(512);
-    float value = values[idx];
-    
-    float currentValue = values[511];
+  int idx = int(uv.x / ar.x * 512.0); // % 512;
+  int startIdx = max(0, idx - 1);
+  int endIdx = min(511, idx + 1);
 
-    // fill area under curve
-    // if (uv.y < value) {
-    //   col.g = 0.5;
-    // }
-    
-    vec2 vpos = vec2(uv.x,  value);
-    
-    // the point
-    //col = vec3(exp_glow(uv - vpos, 0.001));
-    col = vec3(smoothstep(1.0/resolution.y, 0., length(uv - vpos))) * vec3(0.2, 0.96, 0.8);
+  for (int i = startIdx; i <= endIdx; i++) {
+    float x0 = float(i) / 511.0;
+    float x1 = float(i + 1) / 511.0;
+    float y0 = values[i];
+    float y1 = values[i + 1];
+    vec2 p0 = vec2(x0, y0) * ar;
+    vec2 p1 = vec2(x1, y1) * ar;
+    vec2 p = uv.xy;
+    vec2 p0p1 = p1 - p0;
+    vec2 p0p = p - p0;
+    float t = dot(p0p, p0p1) / dot(p0p1, p0p1);
 
-    // red dot at center - placeholder out-of-range indicator
-    if (currentValue > 1.0 - (2.0 / resolution.y)) {
-      col.r += exp_glow(uv - vec2(0.5, 0.5)*ar, 0.02);
-    }
+    if (t < 0.0) t = 0.0;
+    if (t > 1.0) t = 1.0;
 
-    gl_FragColor = vec4(col, 0.5);
+    vec2 projection = p0 + t * p0p1;
+    float d = length(p - projection);
+
+    float alpha = smoothstep(lineThickness, 0., d);
+    col += vec4(vec3(0.5, 0.5, 0.5), alpha) * alpha;
+
+    //col += vec3(smoothstep(lineThickness, 0., d)) * vec3(0.2, 0.96, 0.8);
+  }
+
+  gl_FragColor = vec4(col.rgb, 0.5);
 }
-
