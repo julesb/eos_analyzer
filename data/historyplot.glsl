730b8fd08180b7b214711d7b3c879eb4864c0887
julesb <julesb@users.noreply.github.com>
2023-08-15 21:41:37 +0930

    render plot lines with distance function

--- data/historyplot.glsl
+++ data/historyplot.glsl
@@ -4,16 +4,19 @@ uniform float values[512];
 void main(void) {
   vec2 ar = vec2(resolution.x / resolution.y, 1.0);
   vec2 uv = (gl_FragCoord.xy / resolution) * ar;
-  vec4 col = vec4(0.0, 0.0, 0.0, 1.0);
+  vec3 col = vec3(0.0, 0.0, 0.0);
   
-  //float lineThickness = 1.25 / 300.0;
-  float lineThickness = 1.5 / resolution.y;
+  float lineThickness = 1.1 / resolution.y;
+  //vec3 lineColor = vec3(0.3, 0.4, 0.6);
+  vec3 lineColor = vec3(192./255., 238./255., 1./255.) * 0.5;
 
-  int idx = int(uv.x / ar.x * 512.0); // % 512;
-  int startIdx = max(0, idx - 1);
-  int endIdx = min(511, idx + 1);
+  int idx = int(uv.x / ar.x * 512.0);
+  int startIdx = max(0, idx - 10);
+  int endIdx = min(511, idx + 10);
 
-  for (int i = startIdx; i <= endIdx; i++) {
+  float minDist = 1000;
+
+  for (int i = startIdx; i < endIdx; i++) {
     float x0 = float(i) / 511.0;
     float x1 = float(i + 1) / 511.0;
     float y0 = values[i];
@@ -24,18 +27,15 @@ void main(void) {
     vec2 p0p1 = p1 - p0;
     vec2 p0p = p - p0;
     float t = dot(p0p, p0p1) / dot(p0p1, p0p1);
-
-    if (t < 0.0) t = 0.0;
-    if (t > 1.0) t = 1.0;
-
+    t = max(0.0, min(1.0, t));
     vec2 projection = p0 + t * p0p1;
     float d = length(p - projection);
-
-    float alpha = smoothstep(lineThickness, 0., d);
-    col += vec4(vec3(0.5, 0.5, 0.5), alpha) * alpha;
-
-    //col += vec3(smoothstep(lineThickness, 0., d)) * vec3(0.2, 0.96, 0.8);
+    minDist = min(minDist, d);
   }
 
-  gl_FragColor = vec4(col.rgb, 0.5);
+  float alpha = smoothstep(lineThickness, 0., minDist);
+  col.rgb += lineColor * alpha;
+ 
+
+  gl_FragColor = vec4(col.rgb, 1.0);
 }
